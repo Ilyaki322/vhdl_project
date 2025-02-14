4e61aca50863e3d7324a7b19a031d6d1b3040833
Izumme <ilyakirshtein@gmail.com>
2025-02-14 03:20:48 +0200

    finished connecting control_unit and cpu added rtl drawings

--- control_unit.vhdl
+++ control_unit.vhdl
@@ -11,6 +11,7 @@ entity control_unit is
         clk : in std_logic;
         reset : in std_logic;
 
+        exec_en : in std_logic;
         inst_we : in std_logic; 
         inst_re : in std_logic;
         inst : in std_logic_vector(WIDTH-1 downto 0);
@@ -18,26 +19,33 @@ entity control_unit is
         reg1_we, reg1_re : out std_logic;
         reg2_we, reg2_re : out std_logic;
         reg3_we, reg3_re : out std_logic;
-        reg4_we, reg4_re : out std_logic      
+        reg4_we, reg4_re : out std_logic;
+        
+        main_mem_re : out std_logic;
+        main_mem_we : out std_logic;
 
+        main_data_bus_mux_sel : out std_logic
     );
 end control_unit;
 
 architecture behavioral of control_unit is
 
     constant DECODER_WIDTH : integer := 2;
+    constant REG_DECODER_WIDTH : integer := 4;
 
-    signal unused_data_bus : std_logic_vector(WIDTH-1 downto 0); -- we dont need the data_bus inside the control_unit
+    signal inst_data_bus : std_logic_vector(WIDTH-1 downto 0); -- we dont need the data_bus inside the control_unit
     signal inst_reg_we, inst_reg_re : std_logic;
     signal inst_reg_data : std_logic_vector(WIDTH-1 downto 0);
 
     signal decoder_bus : std_logic_vector((2**DECODER_WIDTH)-1 downto 0);
+    signal reg_address : std_logic_vector((2**REG_DECODER_WIDTH)-1 downto 0);
 
-    signal reg1_we_sig, reg1_re_sig : std_logic;
-    signal reg2_we_sig, reg2_re_sig : std_logic;
-    signal reg3_we_sig, reg3_re_sig : std_logic;
-    signal reg4_we_sig, reg4_re_sig : std_logic;
-    
+    --signal reg1_we_sig, reg1_re_sig : std_logic;
+    --signal reg2_we_sig, reg2_re_sig : std_logic;
+    --signal reg3_we_sig, reg3_re_sig : std_logic;
+    --signal reg4_we_sig, reg4_re_sig : std_logic;
+
+    signal ram_address : std_logic_vector(7 downto 0); -- make generic!
 
     component general_register
     Generic (
@@ -108,22 +116,40 @@ begin
     inst_reg_re <= inst_re;
     inst_reg_data <= inst;
 
-    reg1_we <= reg1_we_sig;
-    reg1_re <= reg1_re_sig;
-    reg2_we <= reg2_we_sig;
-    reg2_re <= reg2_re_sig;
-    reg3_we <= reg3_we_sig;
-    reg3_re <= reg3_re_sig;
-    reg4_we <= reg4_we_sig;
-    reg4_re <= reg4_re_sig;
+    main_data_bus_mux_sel <= decoder_bus(3);
+
+    ram_address <= inst_reg_data(7 downto 0); -- make generic?
+    main_mem_re <= exec_en and (decoder_bus(1) or decoder_bus(3));
+    main_mem_we <= exec_en and decoder_bus(2);
+
+    --reg1_we <= reg1_we_sig;
+    --reg1_re <= reg1_re_sig;
+    --reg2_we <= reg2_we_sig;
+    --reg2_re <= reg2_re_sig;
+    --reg3_we <= reg3_we_sig;
+    --reg3_re <= reg3_re_sig;
+    --reg4_we <= reg4_we_sig;
+    --reg4_re <= reg4_re_sig;
+    reg1_we <= exec_en and (decoder_bus(1) or decoder_bus(3)) and reg_address(0);
+    reg1_re <= exec_en and decoder_bus(2) and reg_address(0);
+    reg2_we <= exec_en and (decoder_bus(1) or decoder_bus(3)) and reg_address(1);
+    reg2_re <= exec_en and decoder_bus(2) and reg_address(1);
+    reg3_we <= exec_en and (decoder_bus(1) or decoder_bus(3)) and reg_address(2);
+    reg3_re <= exec_en and decoder_bus(2) and reg_address(2);
+    reg4_we <= exec_en and (decoder_bus(1) or decoder_bus(3)) and reg_address(3);
+    reg4_re <= exec_en and decoder_bus(2) and reg_address(3);
 
     inst_reg : general_register
     generic map(WIDTH)
-    port map(clk, reset, inst_reg_we, inst_reg_re, unused_data_bus, inst_reg_data);
+    port map(clk, reset, inst_reg_we, inst_reg_re, inst_reg_data, inst_reg_data);
 
     inst_decoder : decoder
     generic map(DECODER_WIDTH) -- change this to a const or generic
     port map(enable, clk, reset, inst_reg_data(WIDTH-1 downto WIDTH-DECODER_WIDTH), decoder_bus);
+
+    reg_decoder : decoder
+    generic map(REG_DECODER_WIDTH)
+    port map(enable, clk, reset, inst_reg_data(WIDTH-5 downto WIDTH-REG_DECODER_WIDTH-5), reg_address);
 
 
 end behavioral;
--- cpu.vhdl
+++ cpu.vhdl
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use work.mux_p;
 
 entity cpu is
@@ -20,6 +21,7 @@ architecture behavioral of cpu is
     signal status : state := fetch;
 
     signal data_bus : std_logic_vector(WIDTH-1 downto 0);
+    signal main_ram_bus : std_logic_vector(WIDTH-1 downto 0);
 
     signal reg1_we, reg1_re : std_logic;
     signal reg1_data : std_logic_vector(WIDTH-1 downto 0);
@@ -33,16 +35,25 @@ architecture behavioral of cpu is
     signal reg4_we, reg4_re : std_logic;
     signal reg4_data : std_logic_vector(WIDTH-1 downto 0);
 
-    signal main_memory_re : std_logic;    
-    signal main_memory_we : std_logic;
+    signal main_memory_re : std_logic := '1';    
+    signal main_memory_we : std_logic := '1';
     signal main_memory_address : std_logic_vector(MEM_SIZE-1 downto 0);
 
     signal instruction_reg_we, instruction_reg_re : std_logic;
     signal instruction_reg_data : std_logic_vector(WIDTH-1 downto 0);
     signal instruction_bus : std_logic_vector(WIDTH-1 downto 0);
 
-    signal instruction_stack_re : std_logic;    
-    signal instruction_stack_we : std_logic;
+    signal instruction_stack_re : std_logic := '1';    
+    signal instruction_stack_we : std_logic := '1';
+
+    signal exec_en : std_logic := '1';
+    signal cu_inst_reg_re : std_logic := '1';    
+    signal cu_inst_reg_we : std_logic := '1';
+
+    signal data_bus_mux_sel : std_logic := '0';
+    signal data_bus_mux_sel_nat : natural;
+
+    signal cu_en : std_logic := '1';
 
     component control_unit
     Generic (
@@ -51,7 +62,22 @@ architecture behavioral of cpu is
     Port(
         enable : in std_logic;
         clk : in std_logic;
-        reset : in std_logic
+        reset : in std_logic;
+
+        exec_en : in std_logic;
+        inst_we : in std_logic; 
+        inst_re : in std_logic;
+        inst : in std_logic_vector(WIDTH-1 downto 0);
+
+        reg1_we, reg1_re : out std_logic;
+        reg2_we, reg2_re : out std_logic;
+        reg3_we, reg3_re : out std_logic;
+        reg4_we, reg4_re : out std_logic;
+        
+        main_mem_re : out std_logic;
+        main_mem_we : out std_logic;
+
+        main_data_bus_mux_sel : out std_logic
     );
     end component;
 
@@ -120,11 +146,12 @@ architecture behavioral of cpu is
 
 begin
 
+    data_bus_mux_sel_nat <= 0 when data_bus_mux_sel = '0' else 1;
     main_mem : ram
     generic map(WIDTH, MEM_SIZE)
-    port map(clk, reset, main_memory_re, main_memory_we, main_memory_address, data_bus);
+    port map(clk, reset, main_memory_re, main_memory_we, main_memory_address, main_ram_bus);
 
-    inst_reg : general_register
+    inst_reg : general_register -- program counter
     generic map(WIDTH)
     port map(clk, reset, instruction_reg_we, instruction_reg_re, data_bus, instruction_reg_data);
 
@@ -148,6 +175,16 @@ begin
     generic map(WIDTH)
     port map(clk, reset, reg4_we, reg4_re, data_bus, reg4_data);
 
+    cu : control_unit
+    generic map(WIDTH)
+    port map(cu_en, clk, reset, exec_en, cu_inst_reg_we, cu_inst_reg_re, instruction_reg_data, 
+    reg1_we, reg1_re, reg2_we, reg2_re, reg3_we, reg3_re, reg4_we, reg4_re, main_memory_re, main_memory_we, data_bus_mux_sel);
+
+    data_bus_mux : mux
+    generic map(WIDTH, 2)
+    port map(enable => enable, clk => clk, selector => data_bus_mux_sel_nat,
+         inputs(0) => main_ram_bus, inputs(1) => instruction_reg_data(7 downto 0), output => data_bus);
+
 
     process (clk) begin
         if reset = '0' then
@@ -156,14 +193,23 @@ begin
         elsif rising_edge(clk) then
             case status is
                 when fetch =>
-                
+                exec_en <= '1';
+                cu_en <= '1';
+                instruction_stack_re <= '0';
+                cu_inst_reg_we <= '0';
+                status <= decode;
                     
                 when decode =>
-                
+                instruction_stack_re <= '1';
+                cu_inst_reg_we <= '1';
+                cu_inst_reg_re <= '0';
+                cu_en <= '0';
+                status <= execute;
                     
                 when execute =>
+                exec_en <= '0';
+                status <= fetch;
                 
-
                 when memory =>
 
 
