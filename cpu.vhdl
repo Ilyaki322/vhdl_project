fc86861c8a70344c81c62376a0ef4132228000e7
Dima <105862960+DimonFiend@users.noreply.github.com>
2025-02-14 08:04:33 +0200

    Update cpu.vhdl

--- cpu.vhdl
+++ cpu.vhdl
@@ -10,7 +10,7 @@ entity cpu is
     Port(
         enable : in std_logic;
         clk : in std_logic;
-        reset : in std_logic
+        reset : in std_logic;
     );
 end cpu;
 
@@ -128,10 +128,12 @@ begin
     generic map(WIDTH)
     port map(clk, reset, instruction_reg_we, instruction_reg_re, data_bus, instruction_reg_data);
 
+    --STACK
     inst_stack : ram
     generic map(WIDTH, MEM_SIZE)
     port map(clk, reset, instruction_stack_re, instruction_stack_we, instruction_reg_data, instruction_bus);
 
+    --=====================Registers===========================================
     reg1 : general_register
     generic map(WIDTH)
     port map(clk, reset, reg1_we, reg1_re, data_bus, reg1_data);
@@ -147,7 +149,7 @@ begin
     reg4 : general_register
     generic map(WIDTH)
     port map(clk, reset, reg4_we, reg4_re, data_bus, reg4_data);
-
+    --=========================================================================
 
     process (clk) begin
         if reset = '0' then
